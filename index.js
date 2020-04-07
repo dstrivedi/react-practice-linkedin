9b1aac5b4cab3fca33c555f90fb3a3ed0370894e
dstrivedi <drashti1118@gmail.com>
2020-04-07 20:34:08 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
+let skiData = {
+  total: 50,
+  powder: 20,
+  backcountry: 10,
+  goal: 100
+};
+
+class SkiDayCounter extends Component {
+  getPercent = decimal => {
+    return decimal * 100 + "%"
   }
-
+  calGoalProgress = (total, goal) => {
+    return this.getPercent(total/goal)
+  }
   render() {
+    const {total, powder, backcountry, goal} = this.props
     return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
+      <section>
+        <div>
+          <p>Total Days: {total}</p>
+        </div>
+        <div>
+          <p>Powder Days: {powder}</p>
+        </div>
+        <div>
+          <p>Backcountry Days: {backcountry}</p>
+        </div>
+        <div>
+          <p>Goal Progress: {this.calGoalProgress(total,goal)}</p>
+        </div>
+      </section>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(
+  <SkiDayCounter
+    total={skiData.total}
+    powder={skiData.powder}
+    backcountry={skiData.powder}
+    goal={skiData.goal}
+  />,
+  document.getElementById("root")
+);
